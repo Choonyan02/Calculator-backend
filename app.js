9edd62a3cbd560709bbeb1655706e2c586947c37
Choonyan02 <choonyan02@gmail.com>
2024-02-16 21:24:37 +0800

    change to urlencoded

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
